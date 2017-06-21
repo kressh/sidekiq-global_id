c613d59f7177951a2a9659d933292c28a4e3f826
Sergey Besedin <kr3ssh@gmail.com>
2017-06-21 19:57:37 +0300

    Ugly fix for retriable jobs with activejob serializer

--- lib/sidekiq/global_id/client_middleware.rb
+++ lib/sidekiq/global_id/client_middleware.rb
@@ -10,8 +10,19 @@ module Sidekiq
       # @param _redis_pool [ConnectionPool]
       # @return [Hash] sidekiq job
       def call(_worker_class, job, _queue, _redis_pool)
-        job['args'] = ActiveJob::Arguments.serialize(job['args'])
+        job = serialize_job(job)
         yield
+      end
+
+      def serialize_job(job)
+        if job.is_a?(Array)
+          return ActiveJob::Arguments.serialize(job)
+        elsif job.is_a?(Hash)
+          job['args'] = ActiveJob::Arguments.serialize(job['args']) if job['retry']
+        end
+        job
+      rescue
+        job
       end
     end
   end
